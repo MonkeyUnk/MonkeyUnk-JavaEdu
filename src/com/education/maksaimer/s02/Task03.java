3f1ab9d3a09a3398593b858a17f0d7cd1adbde99
queen <kp6590014@icloud.com>
2023-07-29 22:18:46 +0300

    Исправлено

--- src/com/education/maksaimer/s02/Task03.java
+++ src/com/education/maksaimer/s02/Task03.java
@@ -3,26 +3,27 @@ package com.education.maksaimer.s02;
 public class Task03 {
 
 
-   public static int typeTetragon(int a, int b, int c, int d) {
-        int sum = (a + b + c + d) % 4;
-        if (sum == 0) {
-            System.out.println("Quadro");
+    public static void typeTetragon(int a, int b, int c, int d) {
 
+
+        if (a == b && a == c && a == d) {
+            System.out.println("Quadro");
         } else {
             System.out.println("Parallepipid");
         }
-        {
-        }
-        return sum;
+
+
+
     }
+
 
     public static void main(String[] args) {
-       typeTetragon(1,1,1,1);
+        typeTetragon(2, 1, 1, 1);
 
 
     }
 
-    }
+}
 
 
 
