717cdc36a58bf6c17b92b10e407968163698f251
queen <kp6590014@icloud.com>
2023-10-18 12:46:32 +0300

    -return + int result

--- src/com/education/andreysumin/oopBox/Box.java
+++ src/com/education/andreysumin/oopBox/Box.java
@@ -51,13 +51,15 @@ public class Box {
     int compare(Box another) {
         double currentVolume = getVolume();
         double anotherVolume = another.getVolume();
+        int result;
         if (currentVolume > anotherVolume) {
-            return 1;
+            result = 1;
         } else if (currentVolume < anotherVolume) {
-            return -1;
+            result = -1;
         } else {
-            return 0;
+            result =  0;
         }
+        return result;
     }
 }
 
