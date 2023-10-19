57a6ce7c42e08f933ab54dc70451de24b90153ac
queen <kp6590014@icloud.com>
2023-10-19 18:00:36 +0300

    Method summa parametrov + const

---
+++ src/com/education/andreysumin/oopMath/Main.java
@@ -0,0 +1,17 @@
+package com.education.andreysumin.oopMath;
+
+import java.util.Arrays;
+
+import static com.education.andreysumin.oopMath.Math.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(multiple(10));
+        System.out.println(area(10));
+        System.out.println(sum(1,2,3,4,5));
+    }
+
+
+
+
+}
